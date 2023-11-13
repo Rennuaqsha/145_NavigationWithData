07845ebbff6c0153a6aaa3f82d092f7abfe90d24
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2023-11-13 23:20:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/praktikum6/data/OrderUiState.kt
@@ -0,0 +1,9 @@
+package com.example.praktikum6.data
+
+import com.example.praktikum6.R
+
+data class OrderUiState(
+    val jumlah: Int=0,
+    val rasa: String="",
+    val harga: String="",
+)
---
+++ app/src/main/java/com/example/praktikum6/data/SumberData.kt
@@ -0,0 +1,10 @@
+package com.example.praktikum6.data
+
+import com.example.praktikum6.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural, R.string.vanilla, R.string.lemon, R.string.milk, R.string.lyche
+
+    )
+}
